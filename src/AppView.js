2a5fb3e91ef69306b9989c8c2bcffffc18d5f1dc
Adriano Grimolizzi <a.grimolizzi@gmail.com>
2020-11-06 18:51:51 +0100

    trying to make it work...

---
+++ src/AppView.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function AppView() {
+    return <div>Odio Tibco!</div>
+}
+
+export default AppView
---
+++ src/Example1.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+export default class Example1 extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <p>You clicked {this.state.count} times!</p>
+                <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+                    Click me!
+                </button>
+            </div>
+        );
+    }
+}
