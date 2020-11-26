5690a62c53e71d1a301e79e980a093a165cb9aa9
Adriano Grimolizzi <a.grimolizzi@gmail.com>
2020-11-26 15:59:13 +0100

    add formik

--- src/App.js
+++ src/App.js
@@ -1,10 +1,83 @@
-import React from 'react';
-import './App.css';
+import { useFormik } from "formik";
+import React from "react";
+import "./App.css";
 
-function App() {
+const validate = (values) => {
+  const errors = {};
+  if (!values.firstName) {
+    errors.firstName = "Required";
+  } else if (values.firstName.length > 15) {
+    errors.firstName = "Must be 15 characters or less";
+  }
+
+  if (!values.lastName) {
+    errors.lastName = "Required";
+  } else if (values.lastName.length > 15) {
+    errors.lastName = "Must be 15 characters or less";
+  }
+
+  if (!values.email) {
+    errors.email = "Required";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = "Invalid email address";
+  }
+
+  return errors;
+};
+
+const App = () => {
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+    },
+    validate,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
   return (
-    <h1>Odio Tibco!</h1>
+    <>
+      <h1>Example Formik App</h1>
+      <form onSubmit={formik.handleSubmit}>
+        <label htmlFor="firstName">First Name</label>
+        <input
+          id="firstName"
+          name="firstName"
+          type="text"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.firstName}
+        />
+        {formik.touched.firstName && formik.errors.firstName ? <div>{formik.errors.firstName}</div> : null}
+        <br />
+        <label htmlFor="lastName">First Name</label>
+        <input
+          id="lastName"
+          name="lastName"
+          type="text"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.lastName}
+        />
+        {formik.touched.lastName && formik.errors.lastName ? <div>{formik.errors.lastName}</div> : null}
+        <br />
+        <label htmlFor="email">Email Address</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.email}
+        />
+        {formik.touched.email && formik.errors.email ? <div>{formik.errors.email}</div> : null}
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import AppContainer from './AppContainer';
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<AppContainer />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
